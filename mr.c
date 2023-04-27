d48975a534d94e8e098ea5177b303b54d2999c83
Gnir1030 <78247475+Gnir1030@users.noreply.github.com>
2023-04-27 07:32:21 +0000

    test case

--- mr.c
+++ mr.c
@@ -9,4 +9,19 @@
 #include "kvlist.h"
 
 void map_reduce(mapper_t mapper, size_t num_mapper, reducer_t reducer,
-                size_t num_reducer, kvlist_t* input, kvlist_t* output) {}
+                size_t num_reducer, kvlist_t* input, kvlist_t* output) {
+                    kvlist_t* list = kvlist_new();
+                    kvlist_iterator_t* itor = kvlist_iterator_new(input);
+                    for(;;){
+                        kvpair_t* pair = kvlist_iterator_next(itor);
+                        if(pair == NULL){
+                            break;
+                        }
+                        mapper(pair, list);
+                    }
+                    kvlist_print(1, list);
+                    kvlist_iterator_free(&itor);
+                    printf("%lu", num_mapper+ num_reducer);
+                    reducer("great",list, output);
+                    kvlist_free(&list);
+    }
