c7fb72efcaeb329217cf138a513b841c81c8d390
Gnir1030 <78247475+Gnir1030@users.noreply.github.com>
2023-04-27 23:59:43 +0000

    split phase

--- mr.c
+++ mr.c
@@ -4,14 +4,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <math.h>
 
 #include "hash.h"
 #include "kvlist.h"
 
 void map_reduce(mapper_t mapper, size_t num_mapper, reducer_t reducer,
                 size_t num_reducer, kvlist_t* input, kvlist_t* output) {
-                    kvlist_t* list = kvlist_new();
+                    //split phase
+                    kvlist_t* lstArray[num_mapper];
+                    size_t input_size = 0;
+                    kvlist_iterator_t* iter = kvlist_iterator_new(input);
+                    while (kvlist_iterator_next(iter) != NULL){
+                        input_size++;
+                    }
+                    kvlist_iterator_free(&iter);
+                    size_t split = input_size/num_mapper;
+
+                    kvlist_iterator_t* iter2 = kvlist_iterator_new(input);
+                    for(size_t i = 0; i < num_mapper; i++){
+                        lstArray[i] = kvlist_new();
+                        size_t counter = split;
+                        while(counter != 0){
+                            kvlist_append(lstArray[i], kvlist_iterator_next(iter2));
+                            counter--;
+                        }
+                    }
+                    kvlist_iterator_free(&iter2);
+
+                    kvlist_print(1, lstArray[0]);
+
                     kvlist_iterator_t* itor = kvlist_iterator_new(input);
+                    kvlist_t* list = kvlist_new();
                     for(;;){
                         kvpair_t* pair = kvlist_iterator_next(itor);
                         if(pair == NULL){
@@ -21,7 +46,7 @@ void map_reduce(mapper_t mapper, size_t num_mapper, reducer_t reducer,
                     }
                     kvlist_print(1, list);
                     kvlist_iterator_free(&itor);
-                    printf("%lu", num_mapper+ num_reducer);
+                    printf("%lu, %lu, %lu\n", num_mapper+ num_reducer, input_size, split);
                     reducer("great",list, output);
                     kvlist_free(&list);
     }
